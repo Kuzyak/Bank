f1797be99b3e189ffe26549f5924b87ddffdf22d
Kuzyak <kuznecov315@gmail.com>
2017-10-02 14:26:04 +0300

    Loans Fix

--- newsite/settings.py
+++ newsite/settings.py
@@ -122,7 +122,7 @@ DATE_FORMAT = 'd E Y'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
--- newsite/wsgi.py
+++ newsite/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-#import djcelery#
 from django.core.wsgi import get_wsgi_application
-#djcelery.setup_loader()#
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsite.settings")
 
