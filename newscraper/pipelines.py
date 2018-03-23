e2b338ed8a30252d1dd886644900cd3bf576b3d4
Kuzyak <kuznecov315@gmail.com>
2018-03-23 20:46:26 +0200

    Bitcoin settings

--- newscraper/pipelines.py
+++ newscraper/pipelines.py
@@ -13,7 +13,35 @@ class NewscraperPipeline(object):
         return item
 
 class DjangoWriterPipeline(object):
+    '''
+    def process_item(self, item, spider):
+      if spider.conf['DO_ACTION']: #Necessary since DDS v.0.9+
+            try:
+                item['news_website'] = spider.ref_object
 
+                checker_rt = SchedulerRuntime(runtime_type='C')
+                checker_rt.save()
+                item['checker_runtime'] = checker_rt
+
+                item.save()
+                spider.action_successful = True
+                dds_id_str = str(item._dds_item_page) + '-' + str(item._dds_item_id)
+                spider.struct_log("{cs}Item {id} saved to Django DB.{ce}".format(
+                    id=dds_id_str,
+                    cs=spider.bcolors['OK'],
+                    ce=spider.bcolors['ENDC']))
+
+            except IntegrityError as e:
+                spider.log(str(e), logging.ERROR)
+                spider.log(str(item._errors), logging.ERROR)
+                raise DropItem("Missing attribute.")
+      else:
+          if not item.is_valid():
+              spider.log(str(item._errors), logging.ERROR)
+              raise DropItem("Missing attribute.")
+
+      return item
+    '''
     def process_item(self, item, spider):
         #print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         if spider.conf['DO_ACTION']: #Necessary since DDS v.0.9+
@@ -37,3 +65,4 @@ class DjangoWriterPipeline(object):
                 spider.log(str(item._errors), logging.ERROR)
                 raise DropItem("Missing attribute.")
         return item
+    #'''
